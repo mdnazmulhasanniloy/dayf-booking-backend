f3df57d4713cfc6aecce575fe3c0ec2a70046bf6
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2026-07-30 12:19:41 +0600

    docker error fixed

--- src/app/utils/firebase.ts
+++ src/app/utils/firebase.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 const serviceAccountPath =
   process.env.FIREBASE_SERVICE_ACCOUNT_PATH ||
-  path.join(process.cwd(), 'firebase-service-account.json');
+  path.join(process.cwd(), 'firebase.json');
 
 const serviceAccount = JSON.parse(
   fs.readFileSync(serviceAccountPath, 'utf8'),
